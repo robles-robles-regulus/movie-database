d5383dd9df87fdc612e2329482c052e8cd84e629
Jonathan Robles <jonathan.robles287@gmail.com>
2021-12-10 12:16:09 -0600

    rendering all movies

--- js/movie-database.js
+++ js/movie-database.js
@@ -1,1 +1,26 @@
-const API_URL = 'https://glen-hexagonal-microraptor.glitch.me/movies.'
+"use strict";
+(()=>{
+    const API_URL = 'https://glen-hexagonal-microraptor.glitch.me/movies';
+    let getAllMovies = () => {
+        return fetch(API_URL).then(resp => resp.json()).catch(err => console.error(err));
+    }
+
+    getAllMovies().then(response => {
+        console.log(response);
+        for (let movie of response) {
+            console.log(movie.title)
+            let output =`
+        <div class="row">
+          <div class="col-4">
+            <img src="${movie.poster}" class="thumbnail">
+          </div>
+          <div class="col-8">
+            <h2>${movie.title}</h2>
+            </div>`
+            $('#movies').append(output)
+        }
+    })
+        .catch((err) => {
+            console.log(err);
+        });
+})();
