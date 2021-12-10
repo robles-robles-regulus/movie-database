c0b232fb52473c7d1fe66f82db5f818c5f8a6133
Jonathan Robles <jonathan.robles287@gmail.com>
2021-12-10 14:09:12 -0600

    changed movies layout

--- js/movie-database.js
+++ js/movie-database.js
@@ -1,5 +1,5 @@
 "use strict";
-(()=>{
+(() => {
     const API_URL = 'https://glen-hexagonal-microraptor.glitch.me/movies';
     let getAllMovies = () => {
         return fetch(API_URL).then(resp => resp.json()).catch(err => console.error(err));
@@ -9,14 +9,16 @@
         console.log(response);
         for (let movie of response) {
             console.log(movie.title)
-            let output =`
-        <div class="row">
-          <div class="col-4">
-            <img src="${movie.poster}" class="thumbnail">
-          </div>
-          <div class="col-8">
-            <h2>${movie.title}</h2>
-            </div>`
+            let output =
+                `<div>
+<div class="card" style="width: 18rem;">
+            <img src="${movie.poster}" height="250" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title text-capatilize">${movie.title}</h5>
+                <p class="card-text">${movie.plot}</p>
+            </div>
+        </div>
+</div>`
             $('#movies').append(output)
         }
     })
